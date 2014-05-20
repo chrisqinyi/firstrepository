7827e540778f30342ca9660922d47c991370f0ae
Ferris <chao_yang2014@163.com>
2014-05-20 23:42:58 +0800

    [Client] Change login layout

--- bee-investment/src/main/resources/client/base/header/headerView.js
+++ bee-investment/src/main/resources/client/base/header/headerView.js
@@ -1,19 +1,19 @@
 App.HeaderView = Ember.View.extend({
-    didInsertElement : function() {
-        $("#loginDialog").dialog({
-            autoOpen : false,
-            width : 400,
-            buttons : [ {
-                text : "Ok",
-                click : function() {
-                    $(this).dialog("close");
-                }
-            }, {
-                text : "Cancel",
-                click : function() {
-                    $(this).dialog("close");
-                }
-            } ]
-        });
-    }
+	didInsertElement : function() {
+		$("#loginDialog").dialog({
+			autoOpen : false,
+			width : 400,
+			buttons : [ {
+				text : "Login",
+				click : function() {
+					$(this).dialog("close");
+				}
+			}, {
+				text : "Cancel",
+				click : function() {
+					$(this).dialog("close");
+				}
+			} ]
+		});
+	}
 });
